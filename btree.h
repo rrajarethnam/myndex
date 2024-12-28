7dd72659ee0ad8fe3af97f64a04b712d69b0843c
Rajeshwar <rajesh_rajr@yahoo.com>
2024-12-28 15:52:26 +0530

    Adapt to Raspberry pi env and Add Delete operation

--- btree.h
+++ btree.h
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <cstring>
 #include <cassert>
+#include <iostream>
 
 template<class Key, class Value> class Btree;
 
@@ -10,13 +11,22 @@ template<class Key, class Value> class Page{
 public:
     virtual Value* getValue(Key key) = 0;
     virtual Key firstKey() = 0;
+    virtual Page* firstPage() = 0;
+    virtual Page* lastPage() = 0;
     virtual void add(Key key, Value value) = 0;
     virtual void add(Key key, Page* page) = 0;
     virtual bool isExternal() = 0;
     virtual Page* next(Key key) = 0;
     virtual bool isFull() = 0;
     virtual Page* split() = 0;
-//    virtual ~Page() = 0;
+    virtual Page* nextPage() = 0;
+    virtual Page* prevPage() = 0;
+    virtual Page* merge(Page* page) = 0;
+    virtual void remove(Key key) = 0;
+    virtual void printKeys() = 0;
+    virtual unsigned int count() = 0;
+    virtual void detach() = 0;
+    virtual ~Page() {};
 };
 
 template<class Key, class Value> class TreePage : public Page<Key, Value>{
@@ -25,6 +35,8 @@ private:
     std::map<Key, TreePage*>* pages;
     std::map<Key, Value>* items;
     unsigned int order;
+    TreePage* _nextPage;
+    TreePage* _prevPage;
 
     friend class Btree<Key, Value>;
 
@@ -40,6 +52,20 @@ public:
     }
     //void close();
 
+    void printKeys(){
+        if (this->bottom) {
+            for(typename std::map<Key, Value>::iterator it = this->items->begin(); it != this->items->end(); it++){
+                std::cout << it->first << ", ";
+            }
+            std::cout << std::endl;
+        } else {
+            for(typename std::map<Key, TreePage*>::iterator it = this->pages->begin(); it != this->pages->end(); it++){
+                std::cout << it->first << ", ";
+            }
+            std::cout << std::endl;
+        }
+    }
+
     Value* getValue(Key key){
         assert(this->isExternal());
         return &(*this->items)[key];
@@ -50,6 +76,22 @@ public:
             return this->items->begin()->first;
         } else {
             return this->pages->begin()->first;
+        }
+    }
+
+    Page<Key, Value>* firstPage(){
+        if (this->bottom) {
+            return NULL;
+        } else {
+            return this->pages->begin()->second;
+        }
+    }
+
+    Page<Key, Value>* lastPage() {
+        if (this->bottom) {
+            return NULL;
+        } else {
+            return this->pages->rbegin()->second;
         }
     }
 
@@ -126,16 +168,90 @@ public:
                 page->add(it->first, it->second);
                 it = this->pages->erase(it);
             }
+            ((TreePage*)this->lastPage())->_nextPage = NULL;
+            ((TreePage*)page->firstPage())->_prevPage = NULL;
         }
+
+        this->_nextPage = page;
+        page->_prevPage = this;
         return page;
+    }
+
+    TreePage* nextPage() {
+        return this->_nextPage;
+    }
+
+    TreePage* prevPage() {
+        return this->_prevPage;
+    }
+
+    unsigned int count(){
+        if (this->bottom) {
+            return this->items->size();
+        } else {
+            return this->pages->size();
+        }
+    }
+
+    void detach(){
+        this->_nextPage = NULL;
+        this->_prevPage = NULL;
+
+        if (this->bottom) {
+            this->items = NULL;
+        } else {
+            this->pages = NULL;
+        }
+    }
+
+    Page<Key, Value>* merge(Page<Key, Value>* page) {
+        TreePage* treePage = (TreePage*)page;
+        if (this->bottom) {
+            for(typename std::map<Key, Value>::iterator it = treePage->items->begin(); it != treePage->items->end(); it++){
+                this->items->insert(*it);
+            }
+        } else {
+            for(typename std::map<Key, TreePage*>::iterator it = treePage->pages->begin(); it != treePage->pages->end(); it++){
+                this->pages->insert(*it);
+            }
+        }
+        this->_nextPage = treePage->_nextPage;
+        if(this->_nextPage != NULL)
+            this->_nextPage->_prevPage = this;
+
+        treePage->detach();
+
+        return this;
+    }
+
+    void remove(Key key){
+        if (this->bottom) {
+            this->items->erase(key);
+        } else {
+            TreePage* page = this->pages->operator[](key);
+            if(page != NULL){
+                if(page->_prevPage != NULL)
+                    page->_prevPage->_nextPage = page->_nextPage;
+                if(page->_nextPage != NULL)
+                    page->_nextPage->_prevPage = page->_prevPage;
+
+                this->pages->erase(key);
+            }
+        }
     }
 
 
     ~TreePage(){
         if (this->bottom) {
-            delete this->items;
+            if(this->items != NULL)
+                delete this->items;
         } else {
-            delete this->pages;
+            if(this->pages != NULL) {
+                for(typename std::map<Key, TreePage*>::iterator it = this->pages->begin(); it != this->pages->end(); it++){
+                    delete it->second;
+                }
+                delete this->pages;
+            }
         }
     }
 };
@@ -145,6 +261,9 @@ private:
     Key* keys;
     Value* values;
     FlatPage** pages;
+    FlatPage* _nextPage;
+    FlatPage* _prevPage;
+
     unsigned int size;
     unsigned int order;
     bool bottom;
@@ -204,6 +323,13 @@ public:
         }
         this->loaded = true;
     }*/
+
+    void printKeys(){
+        for(int i=0; i<this->size; i++){
+            std::cout << this->keys[i] << ", ";
+        }
+        std::cout << std::endl;
+    }
 
     bool isExternal(){
         return this->bottom;
@@ -317,11 +443,93 @@ public:
             this->size = half;
             page->size = half;
         }
+        this->_nextPage = page;
+        page->_prevPage = this;
         return page;
     }
 
+    FlatPage* nextPage() {
+        return this->_nextPage;
+    }
+
+    FlatPage* prevPage() {
+        return this->_prevPage;
+    }
+
+    unsigned int count(){
+        return this->size;
+    }
+
+    Page<Key, Value>* merge(Page<Key, Value>* page) {
+        FlatPage* flatPage = (FlatPage*)page;
+        if (this->bottom) {
+            memcpy(this->keys + this->size, flatPage->keys, flatPage->size * sizeof(Key));
+            memcpy(this->values + this->size, flatPage->values, flatPage->size * sizeof(Value));
+            this->size += flatPage->size;
+        } else {
+            memcpy(this->keys + this->size, flatPage->keys, flatPage->size * sizeof(Key));
+            memcpy(this->pages + this->size, flatPage->pages, flatPage->size * sizeof(FlatPage*));
+            this->size += flatPage->size;
+        }
+        this->_nextPage = flatPage->_nextPage;
+        if(flatPage->_nextPage != NULL)
+            flatPage->_nextPage->_prevPage = this;
+
+        flatPage->detach();
+        delete flatPage;
+        
+        return this;
+    }
+
+    void remove(Key key){
+        if (this->bottom) {
+            int first = 0;
+            int last = this->size - 1;
+            while(first <= last){
+                int mid = first + (last - first) / 2;
+                if (this->keys[mid] == key) {
+                    memmove(this->keys + mid, this->keys + mid + 1, (this->size - mid - 1) * sizeof(Key));
+                    memmove(this->values + mid, this->values + mid + 1, (this->size - mid - 1) * sizeof(Value));
+                    this->size--;
+                    return;
+                }
+                if (this->keys[mid] < key) {
+                    first = mid + 1;
+                } else {
+                    last = mid - 1;
+                }
+            }
+        } else {
+            int first = 0;
+            int last = this->size - 1;
+            while(first <= last){
+                int mid = first + (last - first) / 2;
+                if (this->keys[mid] == key) {
+                    memmove(this->keys + mid, this->keys + mid + 1, (this->size - mid - 1) * sizeof(Key));
+                    memmove(this->pages + mid, this->pages + mid + 1, (this->size - mid - 1) * sizeof(FlatPage*));
+                    this->size--;
+                    return;
+                }
+                if (this->keys[mid] < key) {
+                    first = mid + 1;
+                } else {
+                    last = mid - 1;
+                }
+            }
+        }
+    }
+
+
     Key firstKey(){
         return this->keys[0];
+    }
+
+    Page<Key, Value>* firstPage(){
+        return this->pages[0];
+    }
+
+    Page<Key, Value>* lastPage(){
+        return this->pages[this->size - 1];
     }
 
     bool isFull(){
@@ -355,9 +563,27 @@ public:
         }
     }
 
+    void detach(){
+        this->_nextPage = NULL;
+        this->_prevPage = NULL;
+        if (this->bottom) {
+            this->values = NULL;
+        } else {
+            this->pages = NULL;
+        }
+        this->loaded = false;
+    }
+
     ~FlatPage(){
         delete[] this->keys;
-        delete[] this->values;
+        if(this->bottom){
+            delete[] this->values;
+        } else {
+            for(int i=0; i<this->size; i++){
+                delete this->pages[i];
+            }
+            delete[] this->pages;
+        }
     }
 
 };
@@ -376,6 +602,7 @@ public:
         this->order = order;
         this->root = newPage(true);
         this->root->add(sentinel, sentinelValue);
+        this->height = 1;
     }
 
     Page<Key, Value>* newPage(bool bottom){
@@ -411,6 +638,19 @@ public:
         }
     }
 
+    void deleteKey(Key key){        
+        this->deleteKey(this->root, key);
+        this->n--;
+
+        if(this->root->count() == 1 && this->height > 1){
+            Page<Key, Value>* oldRoot = this->root;
+            this->root = this->root->firstPage();
+            oldRoot->detach();
+            delete oldRoot;
+            this->height--;
+        }
+    }
+
     void put(Page<Key, Value>* page, Key key, Value value){
         if (page->isExternal()) {
             page->add(key, value);
@@ -424,11 +664,58 @@ public:
         }
     }
 
+    void deleteKey(Page<Key, Value>* page, Key key){
+        if (page->isExternal()) {
+            page->remove(key);
+            return;
+        }
+
+        Page<Key, Value>* next = page->next(key);
+        this->deleteKey(next, key);
+        if (next->count() < this->order/2){
+            Page<Key, Value>* prev = next->prevPage();
+            if( prev != NULL) {
+                page->remove(next->firstKey());
+                prev->merge(next);
+                delete next;
+
+                if (prev->isFull()) {
+                    Page<Key, Value>* split_page = prev->split();
+                    page->add(split_page->firstKey(), split_page);
+                }
+            } else {
+                Page<Key, Value>* next_next = next->nextPage();
+                if (next_next != NULL) {
+                    page->remove(next_next->firstKey());
+                    next->merge(next_next);
+                    delete next_next;
+                    
+                    if (next->isFull()) {
+                        Page<Key, Value>* split_page = next->split();
+                        page->add(split_page->firstKey(), split_page);
+                    }
+                } else {
+                    std::cout << "Error: No previous or next page found" << std::endl;
+                }
+            }
+        }
+    }
+
     void save(){
         this->root->save("btree");
     }
 
+    unsigned int count(){
+        return this->n;
+    }
+
+    unsigned int get_height(){
+        return this->height;
+    }
+
+
     ~Btree(){
-        //delete this->root;
+        //Delete the root page
+        delete this->root;
     }
 };
--- test_btree.cpp
+++ test_btree.cpp
@@ -4,14 +4,33 @@
 #include <iostream>
 
 int main(int argc, char* argv[]){
-	Btree<int, int> btree(20, std::numeric_limits<int>::min(), std::numeric_limits<int>::min());
-	for(int i=-1000; i<10000; i++)
+	int begin = 1;
+	int end = 7;
+	Btree<int, int> btree(4, std::numeric_limits<int>::min(), std::numeric_limits<int>::min(), true);
+	for(int i=begin; i<=end; i++){
 		btree.put(i, i);
-		
-	for(int i=-1000; i<10000; i++){
-		assert(*btree.get(i) == i);
-		std::cout << "value:" << *btree.get(i) << std::endl;
+		//std::cout << "inserted:" << i << " and height:" << btree.get_height() << std::endl;
 	}
+	std::cout << "Insertion complete, height:" << btree.get_height() << std::endl;
+
+//	char c;
+//	std::cin >> c;
+		
+	for(int i=begin; i<=end; i++){
+		assert(*btree.get(i) == i);
+		//std::cout << "value:" << *btree.get(i) << std::endl;
+	}
+
+	std::cout << "Retrieval complete" << std::endl;
+
+	for(int i=end; i>=begin; i--){
+		assert(*btree.get(i) == i);
+		btree.deleteKey(i);
+		std::cout << "deleted:" << i << " and height:" << btree.get_height() << std::endl;
+	}
+
+	std::cout << "Deletion complete, height:" << btree.get_height() << std::endl;
+
 		
 	std::cout << "Test Passed" << std::endl;
 }
